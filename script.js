770794f560b49fbb625fee0b20d84ca8d9a1aa51
aalsu <aalsulami35@gmail.com>
2022-06-18 14:17:32 +0300

    wether API

---
+++ script.js
@@ -0,0 +1,12 @@
+const key = "e312e160817990562f2ed61dd30f28c9";
+const requestCity = async (city) => {
+  const baseURL = "http://api.openweathermap.org/data/2.5/weather";
+  const query = `?q=${city}&appid=${key}`;
+
+  //make fetch call (promise call)
+  const response = await fetch(baseURL + query);
+
+  //promise data
+  const data = await response.json();
+  return data;
+};
